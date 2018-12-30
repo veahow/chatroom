b5bdeb8f5ffc701e89444638721f349d51a15a8d
Eahow Chen <veahow@qq.com>
2018-12-31 00:38:35 +0800

    windows console server&client basically completed

--- client/android/app/src/main/java/com/veahow/helloa/MainActivity.java
+++ client/android/app/src/main/java/com/veahow/helloa/MainActivity.java
@@ -1,5 +1,6 @@
 package com.veahow.helloa;
 
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
 
     private Socket socket;    // 套接字
+    private Vibrator vibrator;    // 振动
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);    // 获取按钮实例
         editText = (EditText) findViewById(R.id.edit_text);    // 获取聊天内容实例
         textView = (TextView) findViewById(R.id.content_text_view);    // 服务器测试内容
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
                 // 按钮点击后发送聊天内容到服务器server
 
                 final String inputText = editText.getText().toString();    // 获取聊天内容
+
+                vibrator.vibrate(30);
 
                 new Thread(new Runnable() {
                     @Override
@@ -46,6 +52,10 @@ public class MainActivity extends AppCompatActivity {
                             OutputStream os = socket.getOutputStream();
                             os.write(inputText.getBytes());
                             os.flush();
+
+                            InputStream is = socket.getInputStream();
+
+                            textView.setText(is.read());
 
                             os.close();
                             socket.close();
